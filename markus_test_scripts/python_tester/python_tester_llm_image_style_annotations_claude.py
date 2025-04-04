5f373a5cc52716fc98754fa4c83daf2ecf4600db
Ricky Chen <rickychenyiqi@gmail.com>
2025-04-04 16:12:42 -0400

    shared functions in python tests

--- markus_test_scripts/python_tester/python_tester_llm_image_style_annotations_claude.py
+++ markus_test_scripts/python_tester/python_tester_llm_image_style_annotations_claude.py
@@ -1,49 +1,12 @@
-import json
-import subprocess
 import pytest
 import os
 import os.path 
-import re
-
-        
-def run_llm() -> str:
-    """Run the LLM feedback generator and return results"""
-    llm_command = [
-        "/home/docker/.autotesting/scripts/defaultvenv/bin/python", 
-        "ai-autograding-feedback/main.py",
-        "--submission_type", "jupyter",
-        "--prompt", "image_style_annotations",
-        "--scope", "image",
-        "--assignment", "./",
-        "--question", "Question 5b",
-        "--model", "claude-3.7-sonnet",
-        "--output", "stdout"
-    ]
-
-    # Capture the output from the LLM program
-    llm_result = subprocess.run(llm_command, capture_output=True, text=True)
-    llm_output = llm_result.stdout.strip()
-    return llm_output
-
-def extract_json(response) -> list[dict]:
-    match = re.search(r"\`\`\`json([\S\s]+)\`\`\`", response)
-    if match:
-        return json.loads(match.group(1))
-    return []
-
-DEFAULT_ANNOTATION_WIDTH = 25
-def convert_coordinates(coordinate_pair: list[int]) -> tuple[int]:
-    return (
-        coordinate_pair[0],
-        coordinate_pair[1],
-        coordinate_pair[0] + DEFAULT_ANNOTATION_WIDTH,
-        coordinate_pair[1] + DEFAULT_ANNOTATION_WIDTH
-    )
+from python_tester_llm_image import *
 
 def test_with_markers(request):
     """ Generates LLM response """
     # Run LLM feedback
-    llm_feedback = run_llm()
+    llm_feedback = run_llm("claude-3.7-sonnet", "image_style_annotations")
   
     request.node.add_marker(pytest.mark.markus_message(llm_feedback))
     # Display LLM output in the overall comment
