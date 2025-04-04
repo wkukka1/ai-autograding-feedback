bab0ef80a440ea59cb23eb3196f8aaf3455592ad
Ricky Chen <rickychenyiqi@gmail.com>
2025-04-04 16:12:42 -0400

    shared functions in python tests

--- markus_test_scripts/python_tester/python_tester_llm_image.py
+++ markus_test_scripts/python_tester/python_tester_llm_image.py
@@ -1,34 +1,38 @@
+import json
 import subprocess
-import pytest
-import os
-import os.path 
+import re
 
         
-def run_llm():
-    # Pass test_output as part of the input to the LLM program
+def run_llm(model, prompt) -> str:
+    """Run the LLM feedback generator and return results"""
     llm_command = [
         "/home/docker/.autotesting/scripts/defaultvenv/bin/python", 
         "ai-autograding-feedback/main.py",
         "--submission_type", "jupyter",
-        "--prompt", "image_style",
+        "--prompt", prompt,
         "--scope", "image",
         "--assignment", "./",
         "--question", "Question 5b",
-        "--model", "openai",
+        "--model", model,
         "--output", "stdout"
     ]
 
     # Capture the output from the LLM program
     llm_result = subprocess.run(llm_command, capture_output=True, text=True)
-    llm_output = llm_result.stdout.strip()  # Process output if needed
+    llm_output = llm_result.stdout.strip()
     return llm_output
 
+def extract_json(response) -> list[dict]:
+    match = re.search(r"\`\`\`json([\S\s]+)\`\`\`", response)
+    if match:
+        return json.loads(match.group(1))
+    return []
 
-def test_with_markers(request):
-    """ Generates LLM response """
-    # Run LLM feedback
-    llm_feedback = run_llm()
-  
-    request.node.add_marker(pytest.mark.markus_message(llm_feedback))
-    # Display LLM output in the overall comment
-    request.node.add_marker(pytest.mark.markus_overall_comments(llm_feedback))
+DEFAULT_ANNOTATION_WIDTH = 25
+def convert_coordinates(coordinate_pair: list[int]) -> tuple[int]:
+    return (
+        coordinate_pair[0] - DEFAULT_ANNOTATION_WIDTH // 2,
+        coordinate_pair[1] - DEFAULT_ANNOTATION_WIDTH // 2,
+        coordinate_pair[0] + DEFAULT_ANNOTATION_WIDTH // 2,
+        coordinate_pair[1] + DEFAULT_ANNOTATION_WIDTH // 2
+    )
