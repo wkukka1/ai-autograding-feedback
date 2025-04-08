26cdbccaac730c0de050ae838a18619deeadb3c5
Ricky Chen <rickychenyiqi@gmail.com>
2025-04-08 16:11:56 -0400

    code quality improvements

--- markus_test_scripts/python_tester/python_tester_llm_image_style_annotations_openai.py
+++ markus_test_scripts/python_tester/python_tester_llm_image_style_annotations_openai.py
@@ -1,6 +1,4 @@
 import pytest
-import os
-import os.path 
 from llm_helpers import *
 
 def test_with_markers(request):
@@ -9,20 +7,9 @@ def test_with_markers(request):
     llm_feedback = run_llm(submission="jupyter", question="Question 5b", scope="image", output="stdout",
                            model="openai", prompt="image_style_annotations")
   
+    # Display LLM output in the test output
     request.node.add_marker(pytest.mark.markus_message(llm_feedback))
     # Display LLM output in the overall comment
     request.node.add_marker(pytest.mark.markus_overall_comments(llm_feedback))
-
-    outputs = extract_json(llm_feedback)
-
-    for output in outputs:
-        x1, y1, x2, y2 = convert_coordinates(output["location"])
-        request.node.add_marker(pytest.mark.markus_annotation(
-            type="ImageAnnotation",
-            filename=os.path.relpath("student_submission.png", os.getcwd()),
-            content=output["description"],
-            x1=x1,
-            y1=y1,
-            x2=x2,
-            y2=y2,
-        ))
+    # Add annotations
+    add_image_annotations(request, llm_feedback, "student_submission.png")
