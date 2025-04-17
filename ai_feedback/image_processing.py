b93fe86669d07c6f67b6b3570d56df523c195e95
Ricky Chen <rickychenyiqi@gmail.com>
2025-04-17 16:32:21 -0400

    make question arg optional for image scope

--- ai_feedback/image_processing.py
+++ ai_feedback/image_processing.py
@@ -11,12 +11,14 @@ from .helpers.image_extractor import extract_images
 from .helpers.image_reader import *
 
 
-def encode_image(image_path: os.PathLike):
+def encode_image(image_path: os.PathLike) -> bytes:
+    """Encodes the image found at {image_path} to a base64 string"""
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode("utf-8")
 
 
-def openai_call(message: Message, model: str):
+def openai_call(message: Message, model: str) -> str | None:
+    """Sends a request to OpenAI"""
     # Load environment variables from .env file
     load_dotenv()
     client = OpenAI()
@@ -46,7 +48,9 @@ def openai_call(message: Message, model: str):
     return completion.choices[0].message.content
 
 
-def anthropic_call(message: Message, model: str):
+def anthropic_call(message: Message, model: str) -> str | None:
+    """Sends a request to OpenAI"""
+    # Load environment variables from .env file
     load_dotenv()
     client = Anthropic(api_key=os.getenv("CLAUDE_API_KEY"))
     images = [
@@ -80,7 +84,9 @@ def anthropic_call(message: Message, model: str):
     return message.content[0].text
 
 
-def process_image(args, prompt):
+def process_image(args, prompt: str) -> tuple[str, str]:
+    """Generates feedback for an image submission.
+    Returns the LLM prompt delivered and the returned response."""
     OUTPUT_DIRECTORY = "output_images"
     submission_notebook = Path("./", args.assignment, "student_submission.ipynb")
     solution_notebook = Path("./", args.assignment, "solution.ipynb")
@@ -91,43 +97,59 @@ def process_image(args, prompt):
     if solution_notebook.is_file():
         extract_images(solution_notebook, OUTPUT_DIRECTORY, "solution")
 
-    message = Message(role="user", content=prompt["prompt_content"], images=[])
-
-    # Attachments
-    if prompt.get("include_question_context", False):
-        context = read_question_context(OUTPUT_DIRECTORY, args.question)
-        message.content = message.content.replace(
-            "{context}", "```\n" + context + "\n```"
-        )
-    if prompt.get("include_image_size", False):
-        submission_image_paths = read_submission_images(OUTPUT_DIRECTORY, args.question)
-        submission_image_path = submission_image_paths[
-            0
-        ]  # Only consider one image per question
-        image = PILImage.open(submission_image_path)
-        message.content = message.content.replace(
-            "{image_size}", f"{image.width} by {image.height}"
-        )
-    if prompt.get("include_submission_image", False):
-        submission_image_paths = read_submission_images(OUTPUT_DIRECTORY, args.question)
-        submission_image_path = submission_image_paths[
-            0
-        ]  # Only consider one image per question
-        message.images.append(Image(value=submission_image_path))
-    if prompt.get("include_solution_image", False):
-        solution_image_paths = read_solution_images(OUTPUT_DIRECTORY, args.question)
-        solution_image_path = solution_image_paths[
-            0
-        ]  # Only consider one image per question
-        message.images.append(Image(value=solution_image_path))
-
-    request = f"{message.content}\n\n{[str(image.value) for image in message.images]}"
-    if args.model == Models.OPENAI.value:
-        response = openai_call(message, model="gpt-4o")
-    elif args.model == Models.CLAUDE.value:
-        response = anthropic_call(message, model="claude-3-7-sonnet-20250219")
+    if args.question:
+        questions = [args.question]
     else:
-        response = chat(
-            model=args.model, messages=[message], options={"temperature": 0.33}
-        ).message.content
-    return request, response
+        questions = os.listdir(OUTPUT_DIRECTORY)
+
+    requests: list[str] = []
+    responses: list[str] = []
+    for question in questions:
+        message = Message(role="user", content=prompt["prompt_content"], images=[])
+
+        # Add image attachments and extra information
+        if prompt.get("include_question_context", False):
+            context = read_question_context(OUTPUT_DIRECTORY, question)
+            message.content = message.content.replace(
+                "{context}", "```\n" + context + "\n```"
+            )
+        if prompt.get("include_image_size", False):
+            submission_image_paths = read_submission_images(OUTPUT_DIRECTORY, question)
+            submission_image_path = submission_image_paths[
+                0
+            ]  # Only consider one image per question
+            image = PILImage.open(submission_image_path)
+            message.content = message.content.replace(
+                "{image_size}", f"{image.width} by {image.height}"
+            )
+        if prompt.get("include_submission_image", False):
+            submission_image_paths = read_submission_images(OUTPUT_DIRECTORY, question)
+            submission_image_path = submission_image_paths[
+                0
+            ]  # Only consider one image per question
+            message.images.append(Image(value=submission_image_path))
+        if prompt.get("include_solution_image", False):
+            solution_image_paths = read_solution_images(OUTPUT_DIRECTORY, question)
+            solution_image_path = solution_image_paths[
+                0
+            ]  # Only consider one image per question
+            message.images.append(Image(value=solution_image_path))
+
+        # Prompt the LLM
+        requests.append(
+            f"{message.content}\n\n{[str(image.value) for image in message.images]}"
+        )
+        if args.model == Models.OPENAI.value:
+            responses.append(openai_call(message, model="gpt-4o"))
+        elif args.model == Models.CLAUDE.value:
+            responses.append(
+                anthropic_call(message, model="claude-3-7-sonnet-20250219")
+            )
+        else:
+            responses.append(
+                chat(
+                    model=args.model, messages=[message], options={"temperature": 0.33}
+                ).message.content
+            )
+
+    return "\n\n---\n\n".join(requests), "\n\n---\n\n".join(responses)
