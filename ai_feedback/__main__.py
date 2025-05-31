5454cb930b6d307e38dc193a621732bfeae2dd3d
Will Kukkamalla <will.kukkamalla@mail.utoronto.ca>
2025-05-31 14:17:30 -0400

    added image file arg and changed --test_output arg to have defualt value

--- ai_feedback/__main__.py
+++ ai_feedback/__main__.py
@@ -80,7 +80,7 @@ def main() -> int:
         "--submission",
         type=str,
         required=True,
-        help=HELP_MESSAGES["assignment"]
+        help=HELP_MESSAGES["submission"]
     )
     parser.add_argument(
         "--solution",
@@ -112,7 +112,14 @@ def main() -> int:
         "--test_output",
         type=str,
         required=False,
+        default=None,
         help=HELP_MESSAGES["test_output"])
+    parser.add_argument(
+        "--image",
+        type=str,
+        required=False,
+        help=HELP_MESSAGES["image"]
+    )
     args = parser.parse_args()
 
     prompt_content = ""
