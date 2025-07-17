4b90f3ee4eebe22f68f94b6db7fb1603be90166d
Rolland He <hexucheng2004@gmail.com>
2025-07-17 11:04:02 -0400

    Remove test_submissions/ , tests/ and promptfoo/ files (#26)

--- test_submissions/csc263_opt_connected/fail_submission/fail_submission.py
+++
@@ -1,5 +0,0 @@
-def is_connected(graph):
-    for node in graph:
-        if len(graph[node]) == 0:
-            return False
-    return True
--- test_submissions/gac_example/fail_submission/fail_submission.py
+++
@@ -1,7 +0,0 @@
-def gac_enforce(csp, queue=None):
-    for var in csp.variables:
-        for val in csp.get_domain(var)[:]:
-            for c in csp.get_constraints_with(var):
-                if not c.has_support(var, val):
-                    csp.prune_value(var, val)
-    return True, []
